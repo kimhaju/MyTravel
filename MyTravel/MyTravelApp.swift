616909e0be9369adbc5a4b0b67464035c75db63d
haju <haju0321@gmail.com>
2021-11-04 18:52:46 +0900

    Initial Commit

---
+++ MyTravel/MyTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTravelApp.swift
+//  MyTravel
+//
+//  Created by haju Kim on 2021/11/04.
+//
+
+import SwiftUI
+
+@main
+struct MyTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
